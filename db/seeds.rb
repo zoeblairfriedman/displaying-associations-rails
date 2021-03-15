ca7fcc9fc564283f03d1e1d1a5df08435afd87ff
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-15 17:52:29 -0400

    commit tree 669a5595e0405eee72d9e7d1832775a1e3ff09ef

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movies = Category.create!(name: "Movies")
+movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
